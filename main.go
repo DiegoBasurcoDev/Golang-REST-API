989c42454fe34c8a71aa9eb83fac739d6d5f57e2
Diego Basurco Reyes <d24reyes@gmail.com>
2021-01-18 19:23:47 -0500

    Second Commit

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"log"
 	"encoding/json"
+	"io/ioutil"
+	"strconv"
 	"github.com/gorilla/mux"
 )
 
@@ -25,7 +27,88 @@ var tasks = allTasks {
 }
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if(err != nil) {
+		fmt.Fprint(w, "Insert a Valid Task")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+
+	newTask.ID = len(tasks) +  1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	
+	if(err != nil) {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == taskID {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(task)
+		}
+	}
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	
+	if(err != nil) {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == taskID {
+			tasks = append(tasks[:i], tasks[i + 1:]...)
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprintf(w, "The task with ID %v has been remove succesfully", taskID);
+		}
+	}
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	var updatedTask task
+	
+	if(err != nil) {
+		fmt.Fprintf(w, "Invalid ID")
+		return
+	}
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if(err != nil) {
+		fmt.Fprintf(w, "Please Enter Valid Data")
+	}
+
+	json.Unmarshal(reqBody, &updatedTask)
+
+	for i, task := range tasks {
+		if task.ID == taskID {
+			tasks = append(tasks[:i], tasks[i + 1:]...)
+			updatedTask.ID = taskID
+			tasks = append(tasks, updatedTask)
+
+			fmt.Fprintf(w, "The task with ID %v has been updated successfully", taskID)
+		}
+	}
 }
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
@@ -37,8 +120,12 @@ func main()  {
 
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", indexRoute);
-	router.HandleFunc("/tasks", getTasks)
+	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
